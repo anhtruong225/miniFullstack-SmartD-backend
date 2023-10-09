9f802dbe2fd0455e95f13a67aa3273593bf9aa6a
anhtruong <phanhpompap@gmail.com>
2023-10-09 15:07:31 +0200

    update

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from "express";
 import "./db/server.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import productsRouter from "./routes/productsRouter.js";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
-
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use("/products", productsRouter);
 app.use(errorHandler);
